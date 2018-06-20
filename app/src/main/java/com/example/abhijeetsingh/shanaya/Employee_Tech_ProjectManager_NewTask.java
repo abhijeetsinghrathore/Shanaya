548e7a739510ddce061c226a9028bae7713cf277
hp\Abhijeet Singh <physcphysc@gmail.com>
2018-06-20 14:49:29 +0530

    20-06-2018

---
+++ app/src/main/java/com/example/abhijeetsingh/shanaya/Employee_Tech_ProjectManager_NewTask.java
@@ -0,0 +1,13 @@
+package com.example.abhijeetsingh.shanaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Employee_Tech_ProjectManager_NewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee__tech__project_manager__new_task);
+    }
+}
