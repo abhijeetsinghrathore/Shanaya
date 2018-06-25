40d571accd47e7961f926e4542185cc30947df38
hp\Abhijeet Singh <physcphysc@gmail.com>
2018-06-25 17:30:02 +0530

    25-06-2018: 6th commit

---
+++ app/src/main/java/com/example/abhijeetsingh/shanaya/Employee_Sales_CustomerDetails.java
@@ -0,0 +1,13 @@
+package com.example.abhijeetsingh.shanaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Employee_Sales_CustomerDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee__sales__customer_details);
+    }
+}
