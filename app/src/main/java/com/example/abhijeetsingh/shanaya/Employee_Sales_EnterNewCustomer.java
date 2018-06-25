0a17a8746e7d5425ab52458fa0d6d826d7ee1ded
hp\Abhijeet Singh <physcphysc@gmail.com>
2018-06-25 17:23:11 +0530

    25-06-2018: 5th commit

--- app/src/main/java/com/example/abhijeetsingh/shanaya/Employee_Sales_EnterNewCustomer.java
+++ app/src/main/java/com/example/abhijeetsingh/shanaya/Employee_Sales_EnterNewCustomer.java
@@ -1,13 +1,28 @@
 package com.example.abhijeetsingh.shanaya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Employee_Sales_EnterNewCustomer extends AppCompatActivity {
+
+    Button saveCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee__sales__enter_new_customer);
+
+        saveCustomer=(Button)findViewById(R.id.saveCustomerID);
+
+        saveCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getBaseContext(),Employee_Sales.class);
+                startActivity(i);
+            }
+        });
     }
 }
